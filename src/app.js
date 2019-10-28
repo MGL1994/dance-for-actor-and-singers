fcc192d2a2945cd9e22bcf58db9a58b0214c53b0
Michael Laird <michaelgl1994@gmail.com>
2019-10-28 13:56:49 +0000

    Home, About and Classes pages added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+import Home from './components/pages/Home'
+
+import './style.scss'
 
 class App extends React.Component {
+
   render() {
     return (
-      <h1>Hello World!</h1>
+      <HashRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </HashRouter>
     )
   }
+
 }
 
 ReactDOM.render(
---
+++ src/components/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section id="home" className="hero is-fullheight is-warning">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title has-text-centered">Dance for Actor & Singers</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
