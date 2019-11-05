fd0fa9537d5535f4cac6b1c54ef7993a3aa747c4
Michael Laird <michaelgl1994@gmail.com>
2019-11-05 12:21:22 +0000

    map added

--- src/components/pages/Classes.js
+++ src/components/pages/Classes.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import ReactMapboxGl, { Marker, Popup } from 'react-mapbox-gl'
+
+const mapBoxKey = process.env.mapBoxKey
+
+const Map = ReactMapboxGl({
+  accessToken: mapBoxKey
+})
+const zoom = [12, 16]
 
 const Classes = () => {
   return (
@@ -6,6 +14,16 @@ const Classes = () => {
       <div className="hero-body">
         <div className="container">
           <h1 className="title has-text-centered">Classes</h1>
+
+          <Map
+            style="mapbox://styles/mapbox/streets-v9"
+            zoom={zoom}
+            containerStyle={{
+              height: '600px',
+              width: '100%'
+            }}>
+          </Map>
+
         </div>
       </div>
     </section>
